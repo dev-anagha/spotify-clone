6ab0429e9ea8b411fdc921185f89bd1dcbc51c33
dev-anagha <anagharasale@gmail.com>
2025-06-19 17:54:10 +0530

    Move index.html to root for GitHub Pages

--- src/main/java/com/spotifyclone/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.spotifyclone.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-} 
--- src/main/java/com/spotifyclone/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.spotifyclone.model;
-
-public enum Role {
-    USER,
-    ADMIN
-} 
